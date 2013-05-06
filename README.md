5cb0134a2b681fa2b0f70410cd58ee2302c1629a
Egon Willighagen <egon.willighagen@gmail.com>
2013-05-06 10:42:11 +0200

    My script to create a Derby file for HMDB 3.0

--- README.md
+++ README.md
@@ -1,4 +1,30 @@
-create-bridgedb-hmdb
-====================
+Create BridgeDB Identity Mapping files for HMDB
+===============================================
 
-Create BridgeDB identity mapping files for HMDB
+This groovy script is loosly build on code developed by Martina Kutmon [0]
+and creates a Derby file for BridgeDB [1] for use in PathVisio, etc.
+
+The script has been tested with HMDB 3.0 data from January 2013.
+
+License
+-------
+
+New BSD.
+
+Run the script
+--------------
+
+1. add the jars to your classpath, e.g. on Linux with:
+
+  export CLASSPATH=`ls -1 *.jar | tr '\n' ':'`
+
+2. make sure the HMDB data file is saved as hmdb_metabolites.zip
+
+3. run the script with Groovy:
+
+  groovy hmdb2derby.groovy
+
+4. open the file in PathVisio
+
+0. http://svn.bigcat.unimaas.nl/bridgedbcreator/trunk/src/org/bridgedb/creator/
+1. http://bridgedb.org/
---
+++ hmdb2derby.groovy
@@ -0,0 +1,107 @@
+// export CLASSPATH=`ls -1 *.jar | tr '\n' ':'`
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import groovy.util.slurpersupport.NodeChildren;
+
+import org.bridgedb.IDMapperException;
+import org.bridgedb.DataSource;
+import org.bridgedb.Xref;
+import org.bridgedb.bio.BioDataSource;
+import org.bridgedb.rdb.construct.DBConnector;
+import org.bridgedb.rdb.construct.DataDerby;
+import org.bridgedb.rdb.construct.GdbConstruct;
+import org.bridgedb.rdb.construct.GdbConstructImpl3;
+
+def zipFile = new java.util.zip.ZipFile(new File('hmdb_metabolites.zip'))
+
+GdbConstruct database = GdbConstructImpl3.createInstance(
+  "hmdb_metabolites", new DataDerby(), DBConnector.PROP_RECREATE
+);
+database.createGdbTables();
+database.preInsert();
+
+inchiDS = DataSource.register ("Cin", "InChI").asDataSource()
+inchikeyDS = DataSource.register ("Cik", "InChIKey").asDataSource()
+chemspiderDS = DataSource.register ("Cs", "Chemspider").asDataSource()
+casDS = DataSource.register ("Ca", "CAS").asDataSource()
+pubchemDS = DataSource.register ("Cpc", "PubChem-compound").asDataSource()
+chebiDS = DataSource.register ("Ce", "ChEBI").asDataSource()
+keggDS = DataSource.register ("Ck", "Kegg Compound").asDataSource()
+drugbankDS = DataSource.register ("DrugBank", "DrugBank").asDataSource()
+wikipediaDS = DataSource.register ("Wi", "Wikipedia").asDataSource()
+nugoDS = DataSource.register ("Nw", "NuGO wiki").asDataSource()
+
+String dateStr = new SimpleDateFormat("yyyyMMdd").format(new Date());
+database.setInfo("BUILDDATE", dateStr);
+database.setInfo("DATASOURCENAME", "HMDB3");
+database.setInfo("DATASOURCEVERSION", "hmdb_metabolites_" + dateStr);
+database.setInfo("DATATYPE", "Metabolite");
+database.setInfo("SERIES", "standard_metabolite");
+
+def addXRef(GdbConstruct database, Xref ref, String node, DataSource source) {
+   id = node.trim()
+   println "id: $id"
+   if (id.length() > 0) {
+     ref2 = new Xref(id, source);
+     database.addLink(ref, ref2);
+   }
+}
+
+def addAttribute(GdbConstruct database, Xref ref, String key, String value) {
+   id = value.trim()
+   println "attrib($key): $id"
+   if (id.length() > 0) {
+     database.addAttribute(ref, key, value);
+   }
+}
+
+def cleanKey(String inchikey) {
+   String cleanKey = inchikey
+   if (inchikey.startsWith("InChIKey=")) cleankey = inchikey.tokenize("=").get(1)
+   cleanKey
+}
+
+zipFile.entries().each { entry ->
+   if (!entry.isDirectory()) {
+     println entry.name
+     inputStream = zipFile.getInputStream(entry)
+     def rootNode = new XmlSlurper().parse(inputStream)
+
+     String rootid = rootNode.accession.toString()
+     Xref ref = new Xref(rootid, BioDataSource.HMDB);
+     error = database.addGene(ref);
+
+     // add the synonyms
+     addAttribute(database, ref, "Symbol", rootNode.common_name.toString());
+     rootNode.synonyms.synonym.each { synonym ->
+       addAttribute(database, ref, "Synonym", synonym.toString())
+     }
+     addAttribute(database, ref, "Synonym", rootNode.traditional_iupac.toString());
+     addAttribute(database, ref, "Synonym", rootNode.iupac_name.toString());
+
+     // add the SMILES, InChIKey, etc
+     addAttribute(database, ref, "InChIKey", cleanKey(rootNode.inchkey.toString()));
+     addAttribute(database, ref, "SMILES", rootNode.smiles.toString());
+     addAttribute(database, ref, "BrutoFormula", rootNode.chemical_formula.toString());
+     addAttribute(database, ref, "Taxonomy Parent", rootNode.direct_parent.toString());
+     addAttribute(database, ref, "Monoisotopic Weight", rootNode.monisotopic_moleculate_weight.toString());
+
+     // add external identifiers
+     addXRef(database, ref, rootNode.accession.toString(), BioDataSource.HMDB);
+     addXRef(database, ref, rootNode.cas_registry_number.toString(), casDS);
+     addXRef(database, ref, rootNode.inchi.toString(), inchiDS);
+     addXRef(database, ref, rootNode.chemspider_id.toString(), chemspiderDS);
+     addXRef(database, ref, rootNode.pubchem_compound_id.toString(), pubchemDS);
+     addXRef(database, ref, rootNode.chebi_id.toString(), chebiDS);
+     addXRef(database, ref, rootNode.kegg_id.toString(), keggDS);
+     addXRef(database, ref, rootNode.wikipedia.toString(), wikipediaDS);
+     addXRef(database, ref, rootNode.drugbank_id.toString(), drugbankDS);
+     addXRef(database, ref, rootNode.nugowiki.toString(), nugoDS);
+
+     println "errors: " + error
+  }
+}
+
+database.commit();
+database.finalize();
