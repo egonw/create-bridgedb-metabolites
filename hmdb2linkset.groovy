284b2a64f667e0a11e2607b385627fe13d5482fb
Egon Willighagen <egon.willighagen@gmail.com>
2013-06-18 19:31:58 +0200

    Generalized the script to be able to output multiple mapping files in one go

--- hmdb2linkset.groovy
+++ hmdb2linkset.groovy
@@ -4,94 +4,116 @@ import java.text.SimpleDateFormat;
 import java.util.Date;
 import java.io.*;
 import groovy.util.slurpersupport.NodeChildren;
+import org.codehaus.groovy.runtime.DateGroovyMethods;
 
 def zipFile = new java.util.zip.ZipFile(new File('hmdb_metabolites.zip'))
-def voidFile = new File('hmdb_ls_chemspider.void')
-def lsFile = new File('hmdb_ls_chemspider.ttl')
 
-hmdbNS = "http://identifiers.org/hmdb/"
-chemspiderNSprefix = "http://www.chemspider.com/Chemical-Structure."
-chemspiderNSpostfix = ".rdf#Compound"
-
+// configuring things
+datasets = [
+  [
+    name: "ChemSpider",
+    acronym: "chemspider",
+    targetNSprefix: "http://rdf.chemspider.com/",
+    targetNSpostfix: "",
+    objectsTarget: "ftp://ftp.rsc-us.org/OPS/20130408/void_2013-04-08.ttl#chemSpiderDataset"
+  ]
+]
+    
+def hmdbNS = "http://identifiers.org/hmdb/"
 def predicate = "skos:relatedMatch"
 
-def voidOut = new PrintStream(voidFile.newOutputStream())
-def lsOut = new PrintStream(lsFile.newOutputStream())
+// loop over all data sets
+for (i in 0..0) {
+  def voidFile = new File("hmdb_ls_${datasets[i].acronym}.void")
+  def lsFile = new File("hmdb_ls_${datasets[i].acronym}.ttl")
+  def voidOut = new PrintStream(voidFile.newOutputStream())
+  def lsOut = new PrintStream(lsFile.newOutputStream())
 
-voidOut.println """
-@prefix dcterms: <http://purl.org/dc/terms/> .
-@prefix dctypes: <http://purl.org/dc/dcmitype/> .
-@prefix foaf: <http://xmlns.com/foaf/0.1/> .
-@prefix pav: <http://purl.org/pav/> .
-@prefix rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#> .
-@prefix rdfs: <http://www.w3.org/2000/01/rdf-schema#> .
-@prefix voag: <http://voag.linkedmodel.org/schema/voag#> .
-@prefix void: <http://rdfs.org/ns/void#> .
-@prefix xsd: <http://www.w3.org/2001/XMLSchema#> .
-@prefix dcat: <http://www.w3.org/ns/dcat#> .
-@prefix skos: <http://www.w3.org/2004/02/skos/core#> .
-@prefix dul: <http://www.ontologydesignpatterns.org/ont/dul/DUL.owl#> .
-@prefix freq: <http://purl.org/cld/freq/> .
-@prefix cheminf: <http://semanticscience.org/resource/> .
+  dateTime = new Date()
+  current_date = DateGroovyMethods.format(dateTime, "yyyy-MM-dd'T'HH:mm:ss");
 
-@prefix : <#> .
+  voidOut.println """
+  @prefix dcterms: <http://purl.org/dc/terms/> .
+  @prefix dctypes: <http://purl.org/dc/dcmitype/> .
+  @prefix foaf: <http://xmlns.com/foaf/0.1/> .
+  @prefix pav: <http://purl.org/pav/> .
+  @prefix rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#> .
+  @prefix rdfs: <http://www.w3.org/2000/01/rdf-schema#> .
+  @prefix voag: <http://voag.linkedmodel.org/schema/voag#> .
+  @prefix void: <http://rdfs.org/ns/void#> .
+  @prefix xsd: <http://www.w3.org/2001/XMLSchema#> .
+  @prefix dcat: <http://www.w3.org/ns/dcat#> .
+  @prefix skos: <http://www.w3.org/2004/02/skos/core#> .
+  @prefix dul: <http://www.ontologydesignpatterns.org/ont/dul/DUL.owl#> .
+  @prefix freq: <http://purl.org/cld/freq/> .
+  @prefix cheminf: <http://semanticscience.org/resource/> .
 
-<> a void:DatasetDescription;
-  dcterms:title "A VoID Description of the HMDB 2 ChemSpider LinkSet" ;
-  dcterms:description "VoID file describing the link set between HMDB and ChemSpider." ;
-  pav:createdBy <http://egonw.github.com/#me> ;
-  pav:createdOn "2013-05-27T18:49:00Z"^^xsd:dataTime ;
-  pav:lastUpdateOn "2013-05-29T12:57:00Z"^^xsd:dateTime ;
-  foaf:primaryTopic :LS .
+  @prefix : <#> .
 
-:LS a void:Linkset ;
-  dcterms:title "HMDB 2 ChemSpider LinkSet" ;
-  dcterms:description "A link set with links between HMDB and ChemSpider entries."@en;
-  dcterms:license <http://www.hmdb.ca/citing>;
-  pav:version "3.0.1"^^xsd:string ;
-  pav:createdBy <http://egonw.github.com/#me> ;
-  pav:createdOn "2013-05-29T10:02:00Z"^^xsd:dateTime ;
-  void:linkPredicate $predicate ;
-  dul:expresses cheminf:CHEMINF_000044 ;
-  void:subjectsTarget :HMDB ;
-  void:objectsTarget <ftp://ftp.rsc-us.org/OPS/20130408/void_2013-04-08.ttl#chemSpiderDataset> ;
-  pav:authoredBy <http://www.hmdb.ca/>;
-  pav:authoredOn "2013-05-29T10:02:00Z"^^xsd:dateTime ;
+  <> a void:DatasetDescription;
+    dcterms:title "A VoID Description of the HMDB 2 ${datasets[i].name} LinkSet" ;
+    dcterms:description "VoID file describing the link set between HMDB and ${datasets[i].name}." ;
+    pav:createdBy <http://egonw.github.com/#me> ;
+    pav:createdOn "2013-05-27T18:49:00Z"^^xsd:dataTime ;
+    pav:lastUpdateOn "${current_date}Z"^^xsd:dateTime ;
+    foaf:primaryTopic :LS .
 
-:HMDB a void:Dataset ;
-  void:subset :LS ;
-  dcterms:title "Human Metabolite Database";
-  dcterms:description "The Human Metabolite Database (HMDB) is a database with information about metabolites, drugs and (other) xenobiotics found in the human organism. It is described in this paper: Wishart DS, Jewison T, Guo AC, Wilson M, Knox C, et al., HMDB 3.0—The Human Metabolome Database in 2013. Nucleic Acids Res. 2013. Jan 1;41(D1):D801-7."@en;
-  dcterms:license <http://www.hmdb.ca/citing>;
-  foaf:homepage <http://www.hmdb.ca/>;
-  foaf:page <http://www.hmdb.ca/>;
-  pav:retrievedBy <http://egonw.github.com/#me> ;
-  pav:retrievedFrom <http://www.hmdb.ca/downloads> ;
-  pav:retrievedOn "2013-05-27T18:49:00Z"^^xsd:dateTime ;
-  void:uriSpace <$hmdbNS> ;
-  void:dataDump <http://www.bigcat.unimaas.nl/~egonw/hmdb/3.0.0/hmdb_ls_chemspider.ttl> ;
-  void:exampleResource <http://identifiers.org/hmdb/HMDB00005> ;
-  voag:frequencyOfChange <http://purl.org/cld/freq/irregular> ;
-  pav:version "3".
-"""
+  :LS a void:Linkset ;
+    dcterms:title "HMDB 2 ${datasets[i].name} LinkSet" ;
+    dcterms:description "A link set with links between HMDB and ${datasets[i].name} entries."@en;
+    dcterms:license <http://www.hmdb.ca/citing>;
+    pav:version "3.0.1"^^xsd:string ;
+    pav:createdBy <http://egonw.github.com/#me> ;
+    pav:createdOn "${current_date}Z"^^xsd:dateTime ;
+    void:linkPredicate $predicate ;
+    dul:expresses cheminf:CHEMINF_000044 ;
+    void:subjectsTarget :HMDB ;
+    void:objectsTarget <${datasets[i].objectsTarget}> ;
+    pav:authoredBy <http://www.hmdb.ca/>;
+    pav:authoredOn "2013-05-29T10:02:00Z"^^xsd:dateTime ;
 
-def tripleCount = 0
-zipFile.entries().each { entry ->
-  if (!entry.isDirectory()) {
-    inputStream = zipFile.getInputStream(entry)
-    def rootNode = new XmlSlurper().parse(inputStream)
+  :HMDB a void:Dataset ;
+    void:subset :LS ;
+    dcterms:title "Human Metabolite Database";
+    dcterms:description "The Human Metabolite Database (HMDB) is a database with information about metabolites, drugs and (other) xenobiotics found in the human organism. It is described in this paper: Wishart DS, Jewison T, Guo AC, Wilson M, Knox C, et al., HMDB 3.0—The Human Metabolome Database in 2013. Nucleic Acids Res. 2013. Jan 1;41(D1):D801-7."@en;
+    dcterms:license <http://www.hmdb.ca/citing>;
+    foaf:homepage <http://www.hmdb.ca/>;
+    foaf:page <http://www.hmdb.ca/>;
+    pav:retrievedBy <http://egonw.github.com/#me> ;
+    pav:retrievedFrom <http://www.hmdb.ca/downloads> ;
+    pav:retrievedOn "2013-05-27T18:49:00Z"^^xsd:dateTime ;
+    void:uriSpace <$hmdbNS> ;
+    void:dataDump <http://www.bigcat.unimaas.nl/~egonw/hmdb/3.0.0/hmdb_ls_${datasets[i].acronym}.ttl> ;
+    void:exampleResource <http://identifiers.org/hmdb/HMDB00005> ;
+    voag:frequencyOfChange <http://purl.org/cld/freq/irregular> ;
+    pav:version "3".
+  """
 
-    String rootid = rootNode.accession.toString()
+  def tripleCount = 0
+  zipFile.entries().each { entry ->
+    if (!entry.isDirectory()) {
+      inputStream = zipFile.getInputStream(entry)
+      def rootNode = new XmlSlurper().parse(inputStream)
 
-    // add external identifiers
-    tripleCount++
-    lsOut.println "<" + hmdbNS + rootid + "> $predicate <" +
-          chemspiderNSprefix + rootNode.chemspider_id.toString() + chemspiderNSpostfix +
-          "> ."
+      String rootid = rootNode.accession.toString()
+
+      // add external identifiers
+      tripleCount++
+      lsOut.println "<" + hmdbNS + rootid + "> $predicate <" +
+	    datasets[i].targetNSprefix + rootNode.chemspider_id.toString() + datasets[i].targetNSpostfix +
+	    "> ."
+      if (tripleCount > 5) { 
+	voidOut.println ":LS void:triples $tripleCount ."
+
+	voidOut.close()
+	lsOut.close()
+	System.exit(0)
+      }
+    }
   }
+
+  voidOut.println ":LS void:triples $tripleCount ."
+
+  voidOut.close()
+  lsOut.close()
 }
-
-voidOut.println ":LS void:triples $tripleCount ."
-
-voidOut.close()
-lsOut.close()
