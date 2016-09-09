d7d5ee87b086712e234e2137cce9c3c28f02f0ed
Egon Willighagen <egon.willighagen@gmail.com>
2016-09-09 09:52:22 +0200

    Bumped the version

--- hmdb2derby.groovy
+++ hmdb2derby.groovy
@@ -53,7 +53,7 @@ wikipediaDS = BioDataSource.WIKIPEDIA
 String dateStr = new SimpleDateFormat("yyyyMMdd").format(new Date());
 database.setInfo("BUILDDATE", dateStr);
 database.setInfo("DATASOURCENAME", "HMDB-CHEBI-WIKIDATA");
-database.setInfo("DATASOURCEVERSION", "HMDB3.6-CHEBI140-WIKIDATA20160604" + dateStr);
+database.setInfo("DATASOURCEVERSION", "HMDB3.6-CHEBI142-WIKIDATA20160907" + dateStr);
 database.setInfo("DATATYPE", "Metabolite");
 database.setInfo("SERIES", "standard_metabolite");
 
